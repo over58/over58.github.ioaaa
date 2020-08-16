4c5fbf4fab6c1a7bcb85b0645afb34178a5d3a16
over <26474133+over58@users.noreply.github.com>
2020-08-16 18:26:10 +0800

    update artcile

--- deploy.js
+++ deploy.js
@@ -4,15 +4,13 @@ let chalk  = require('chalk')
 let commands = [
   'git add .',
   'git commit -m "update artcile"',
-  'git push origin master',
-  'hexo d -g'
+  'git push origin myblog'
 ]
 
 let comments = [
   "添加暂存...",
   "编辑提交信息...",
-  "提交代码中...",
-  "开启部署..."
+  "提交代码中..."
 ]
 commands.forEach((command, index) => {
   console.log(chalk.yellowBright(comments[index]))
